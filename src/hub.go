80cef36aad396399c1029ea17063fff47c777c22
qubard <qubard@gmail.com>
2020-04-01 18:21:38 -0400

    go fmt

--- src/hub.go
+++ src/hub.go
@@ -2,25 +2,25 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
-    "github.com/qubard/claack-go/messages/handlers"
+	"github.com/qubard/claack-go/messages/handlers"
 )
 
 type Hub struct {
-	clients map[*Client]bool
-	broadcast chan []byte
-	register chan *Client
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
 	unregister chan *Client
-    handlerBus *handlers.HandlerBus
-    upgrader *websocket.Upgrader
+	handlerBus *handlers.HandlerBus
+	upgrader   *websocket.Upgrader
 }
 
 func CreateHub() *Hub {
-	return &Hub {
+	return &Hub{
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
-        handlerBus: handlers.CreateHandlerBus(),
+		handlerBus: handlers.CreateHandlerBus(),
 	}
 }
 
@@ -34,16 +34,16 @@ func (hub *Hub) run() {
 				delete(hub.clients, client)
 				close(client.send)
 			}
-        // Loop over all the clients and broadcast any messages 
+			// Loop over all the clients and broadcast any messages
 		case message := <-hub.broadcast:
 			for client := range hub.clients {
 				select {
-                    case client.send <-message:
-                    default:
-                        // Close the socket
-                        close(client.send)
-                        // Remove the client from the map
-                        delete(hub.clients, client)
+				case client.send <- message:
+				default:
+					// Close the socket
+					close(client.send)
+					// Remove the client from the map
+					delete(hub.clients, client)
 				}
 			}
 		}
--- src/main.go
+++ src/main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-    "net/http"
-    "log"
-    
-    "github.com/qubard/claack-go/messages/types"
-    "github.com/gorilla/websocket"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/qubard/claack-go/messages/types"
 )
 
-var allowedHosts = map[string]bool{ 
-    "http://localhost:3000": true,
+var allowedHosts = map[string]bool{
+	"http://localhost:3000": true,
 }
 
 func filterOrigin(r *http.Request) bool {
-    host := r.Header.Get("Origin")
-    _, allowed := allowedHosts[host]
-    return allowed
+	host := r.Header.Get("Origin")
+	_, allowed := allowedHosts[host]
+	return allowed
 }
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-    CheckOrigin: filterOrigin,
+	CheckOrigin:     filterOrigin,
 }
 
 func main() {
-    // Create and use our websocket app
-    claack := CreateApp()
-    claack.SetUpgrader(&upgrader)
-    claack.ParseFlags()
-    
-    claack.hub.handlerBus.RegisterHandler(types.InitSocket, func(msg interface{}) {
-        log.Println("Init Socket message", msg)
-    })
-    
-    claack.Start()
-    claack.StartHTTP("/ws")
+	// Create and use our websocket app
+	claack := CreateApp()
+	claack.SetUpgrader(&upgrader)
+	claack.ParseFlags()
+
+	claack.hub.handlerBus.RegisterHandler(types.InitSocket, func(msg interface{}) {
+		log.Println("Init Socket message", msg)
+	})
+
+	claack.Start()
+	claack.StartHTTP("/ws")
 }
